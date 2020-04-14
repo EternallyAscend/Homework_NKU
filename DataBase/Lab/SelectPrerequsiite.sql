a2ea698ba71b4a872ff4071bf6ffe3ab8769a1c7
EternallyAscend <52477444+EternallyAscend@users.noreply.github.com>
2020-04-15 00:03:32 +0800

    Update SelectPrerequsiite.sql

--- DataBase/Lab/SelectPrerequsiite.sql
+++ DataBase/Lab/SelectPrerequsiite.sql
@@ -1,86 +1,38 @@
 set global log_bin_trust_function_creators=1;
-
-drop function if exists selectPrerequisite;
-drop table result;
-
+SET GLOBAL max_sp_recursion_depth =100;
+drop procedure if exists selectPrerequisite;
+drop table prerequisiteResult;
+drop table prerequisiteTemp;
+drop table prerequisiteAdd;
+create temporary table if not exists prerequisiteResult(id varchar(8));
+create temporary table if not exists prerequisiteTemp (id varchar(8));
 delimiter //
-create function selectPrerequisite(name varchar(8)) returns varchar(8)
+create procedure selectPrerequisite(name varchar(8))
 begin
-	declare res varchar(8);
 	declare counter int;
-	declare i int;
 	declare cache varchar(8);
-	create temporary table if not exists Result(id varchar(8));
-	drop temporary table if exists temp;
-	create temporary table temp (id varchar(8));
-	insert into temp select a.id from (select prereq_id as id from prereq where course_id=name) as a left join (select id from result) as r on a.id=r.id where r.id is NULL; 
-	insert into result select id from temp;
-	set counter=0;
-	set i=1;
-	select count(*) from temp into counter;
-	while counter>=i do
+	declare start int;
+	declare end int;
+	create temporary table if not exists prerequisiteResult(id varchar(8));
+	create temporary table if not exists prerequisiteTemp (id varchar(8));
+    drop table if exists prerequisiteAdd;
+    create temporary table prerequisiteAdd(id varchar(8));
+	select count(*) from prerequisiteTemp into start;
+	insert into prerequisiteAdd select a.id from (select prereq_id as id from prereq where course_id=name) as a left join (select id from prerequisiteResult) as r on a.id=r.id where r.id is NULL; 
+    insert into prerequisiteTemp select id from prerequisiteAdd;    
+	insert into prerequisiteResult select id from prerequisiteAdd;
+	select count(*) from prerequisiteTemp into end;
+	set counter=start;
+	while counter<end do
 		set cache='';
-		set res='';
-		select id from temp limit i,1 into cache;
-		select selectPrerequisite(cache) into res;
-		set i=i+1;
-    end while;
-    set res=0;
-    select count(*) from result into res;
-	return res;
-end //
-delimiter ;
-
-select selectPrerequisite(292);
-
-
--- 
---	select selectPrerequisite(a.cache) from (select prereq_id as cache from prereq where course_id=name as a) where a.;
-
--- select id from temp limit i,1 into cache;
--- selectPrerequisite(cache);
--- 
---		selectPrerequisite(cache);
-
---     select count(*) from result into res;
-
---		select selectPrerequisite(cache);
-
--- 292-> 399,664,489,704,416;
-
-
-
-set global log_bin_trust_function_creators=1;
-
-drop function if exists selectPrerequisite;
-drop table result;
-
-delimiter //
-create function selectPrerequisite(name varchar(8)) returns varchar(8)
-begin
-	declare res varchar(8);
-	declare counter int;
-	declare i int;
-	declare cache varchar(8);
-	create temporary table if not exists result(id varchar(8));
-	drop temporary table if exists temp;
-	create temporary table temp (id varchar(8));
-	insert into temp select a.id from (select prereq_id as id from prereq where course_id=name) as a left join (select id from result) as r on a.id=r.id where r.id is NULL; 
-	insert into result select id from temp;
-	set counter=0;
-	set i=1;
-	select count(*) from temp into counter;
-	while counter>=i do
-		set cache='';
-		set res='';
-		select id from temp limit i,1 into cache;
+		select id from prerequisiteTemp limit counter,1 into cache;
 		call selectPrerequisite(cache);
-		set i=i+1;
+		set counter=counter+1;
     end while;
-    set res=0;
-    select count(*) from result into res;
-	return res;
 end //
 delimiter ;
 
-select selectPrerequisite(292);
+call selectPrerequisite(292);
+
+select * from prerequisiteResult;
+select * from prerequisiteTemp;
